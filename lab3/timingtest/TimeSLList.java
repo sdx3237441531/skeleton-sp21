8fbf4a642e13825af082e544b0fb3657864c38e1
sdx3237441531 <3237441531@qq.com>
2025-12-20 17:19:23 +0800

    Update TimeSLList.java

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -1,4 +1,5 @@
 package timingtest;
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Stopwatch;
 
 /**
@@ -22,7 +23,95 @@ public class TimeSLList {
     }
 
     public static void timeGetLast() {
-        // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+
+        SLList<Integer> test1 = new SLList<>();
+        for (int i = 0; i < 1000; i++) {
+            test1.addLast(i);
+        }
+        Stopwatch sw1 = new Stopwatch();
+        for (int i = 0; i < 10000; i++) {
+            test1.addLast(i);
+        }
+        Ns.addLast(1000);
+        times.addLast(sw1.elapsedTime());
+        opCounts.addLast(10000);
+
+        SLList<Integer> test2 = new SLList<>();
+        for (int i = 0; i < 2000; i++) {
+            test2.addLast(i);
+        }
+        Stopwatch sw2 = new Stopwatch();
+        for (int i = 0; i < 10000; i++) {
+            test2.addLast(i);
+        }
+        Ns.addLast(2000);
+        times.addLast(sw2.elapsedTime());
+        opCounts.addLast(10000);
+
+        SLList<Integer> test3 = new SLList<>();
+        for (int i = 0; i < 4000; i++) {
+            test3.addLast(i);
+        }
+        Stopwatch sw3 = new Stopwatch();
+        for (int i = 0; i < 10000; i++) {
+            test3.addLast(i);
+        }
+        Ns.addLast(4000);
+        times.addLast(sw3.elapsedTime());
+        opCounts.addLast(10000);
+
+        SLList<Integer> test4 = new SLList<>();
+        for (int i = 0; i < 8000; i++) {
+            test4.addLast(i);
+        }
+        Stopwatch sw4 = new Stopwatch();
+        for (int i = 0; i < 10000; i++) {
+            test4.addLast(i);
+        }
+        Ns.addLast(8000);
+        times.addLast(sw4.elapsedTime());
+        opCounts.addLast(10000);
+
+        SLList<Integer> test5 = new SLList<>();
+        for (int i = 0; i < 16000; i++) {
+            test5.addLast(i);
+        }
+        Stopwatch sw5 = new Stopwatch();
+        for (int i = 0; i < 10000; i++) {
+            test5.addLast(i);
+        }
+        Ns.addLast(16000);
+        times.addLast(sw5.elapsedTime());
+        opCounts.addLast(10000);
+
+        SLList<Integer> test6 = new SLList<>();
+        for (int i = 0; i < 32000; i++) {
+            test6.addLast(i);
+        }
+        Stopwatch sw6 = new Stopwatch();
+        for (int i = 0; i < 10000; i++) {
+            test6.addLast(i);
+        }
+        Ns.addLast(32000);
+        times.addLast(sw6.elapsedTime());
+        opCounts.addLast(10000);
+
+        SLList<Integer> test7 = new SLList<>();
+        for (int i = 0; i < 64000; i++) {
+            test7.addLast(i);
+        }
+        Stopwatch sw7 = new Stopwatch();
+        for (int i = 0; i < 10000; i++) {
+            test7.addLast(i);
+        }
+        Ns.addLast(64000);
+        times.addLast(sw7.elapsedTime());
+        opCounts.addLast(10000);
+
+        printTimingTable(Ns, times, opCounts);
     }
 
 }
