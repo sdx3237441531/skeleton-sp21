c8b57ced280959f5d82cd8c69c48b7ecd09d1b76
sdx3237441531 <3237441531@qq.com>
2025-12-24 11:32:17 +0800

    Create IntegerComparator.java

---
+++ proj1/deque/IntegerComparator.java
@@ -0,0 +1,9 @@
+package deque;
+
+import java.util.Comparator;
+
+public class IntegerComparator implements Comparator<Integer> {
+    public int compare(Integer o1, Integer o2) {
+        return o1.compareTo(o2);
+    }
+}
