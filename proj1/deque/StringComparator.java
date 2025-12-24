6a1a18d729371dda80c8ffdbf5df9af30df1eaf8
sdx3237441531 <3237441531@qq.com>
2025-12-24 11:31:59 +0800

    Create StringComparator.java

---
+++ proj1/deque/StringComparator.java
@@ -0,0 +1,9 @@
+package deque;
+
+import java.util.Comparator;
+
+public class StringComparator implements Comparator<String> {
+    public int compare(String o1, String o2) {
+        return o1.compareTo(o2);
+    }
+}
