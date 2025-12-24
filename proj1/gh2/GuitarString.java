10da31641bad6131d2fa8a0a31fbe2e1067dbf70
sdx3237441531 <3237441531@qq.com>
2025-12-24 14:00:03 +0800

    Update GuitarString.java

--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -1,9 +1,7 @@
 package gh2;
 
-import deque.ArrayDeque;
 import deque.Deque;
 import deque.LinkedListDeque;
-// TODO: maybe more imports
 
 //Note: This file will not compile until you complete the Deque implementations
 public class GuitarString {
@@ -20,10 +18,6 @@ public class GuitarString {
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this division operation into an int. For
-        //       better accuracy, use the Math.round() function before casting.
-        //       Your should initially fill your buffer array with zeros.
         buffer = new LinkedListDeque<>();
         int size = (int)Math.round(SR / frequency);
         for (int i = 0; i < size; i += 1) {
@@ -33,10 +27,6 @@ public class GuitarString {
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in buffer, and replace with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
-        //
         //       Make sure that your random numbers are different from each
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
@@ -57,16 +47,11 @@ public class GuitarString {
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       **Do not call StdAudio.play().**
-        buffer.addLast((buffer.get(0) + buffer.get(1)) / 2 * DECAY);
+        buffer.addLast((buffer.removeFirst() + buffer.get(0)) / 2 * DECAY);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing
         return buffer.get(0);
     }
 }
-// TODO: Remove all comments that say TODO when you're done.
