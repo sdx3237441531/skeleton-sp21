e5073c41882fa89b7a47a4f25cb81d424974ba30
sdx3237441531 <3237441531@qq.com>
2025-12-29 18:00:06 +0800

    Update Dog.java

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -7,10 +7,10 @@ import static capers.Utils.*;
 /** Represents a dog that can be serialized.
  * @author TODO
 */
-public class Dog { // TODO
+public class Dog implements Serializable{ // TODO
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
+    static final File DOG_FOLDER = Utils.join(CapersRepository.CAPERS_FOLDER, "dogs"); // TODO (hint: look at the `join`
                                          //      function in Utils)
 
     /** Age of dog. */
@@ -40,7 +40,9 @@ public class Dog { // TODO
      */
     public static Dog fromFile(String name) {
         // TODO (hint: look at the Utils file)
-        return null;
+        File dogFile = Utils.join(Dog.DOG_FOLDER, name);
+        Dog d = Utils.readObject(dogFile, Dog.class);
+        return d;
     }
 
     /**
@@ -57,6 +59,16 @@ public class Dog { // TODO
      */
     public void saveDog() {
         // TODO (hint: don't forget dog names are unique)
+        File dogFile = Utils.join(Dog.DOG_FOLDER, name);
+        Utils.writeObject(dogFile, this);
+        if (dogFile.exists()) {
+            Dog d = Utils.readObject(dogFile, Dog.class);
+            if (d.name.equals(name) && d.breed.equals(breed)) {
+                Utils.writeObject(dogFile, this);
+            }
+        } else {
+            Utils.writeObject(dogFile, this);
+        }
     }
 
     @Override
