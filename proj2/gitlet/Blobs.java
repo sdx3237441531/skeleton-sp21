2e6c82b4bec7bb0c7d21110366ea0bc17238c338
sdx3237441531 <3237441531@qq.com>
2026-01-13 09:56:45 +0800

    Create Blobs.java

---
+++ proj2/gitlet/Blobs.java
@@ -0,0 +1,32 @@
+package gitlet;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Blobs implements Serializable {
+    private String ID; // SHA-1 ID
+    private String fileName; //文件名
+    private String fileContent; // 文件内容
+
+    public Blobs(String fileName, String fileContent) {
+        this.fileName = fileName;
+        this.fileContent = fileContent;
+        //创建ID
+        List<Object> l = new ArrayList<>();
+        l.add(this.fileName);
+        l.add(this.fileContent);
+        ID = Utils.sha1(l);
+    }
+
+    //获取ID
+    public String getID() {
+        return ID;
+    }
+
+    //获取文件名
+    public String getFileName() {
+        return fileName;
+    }
+}
