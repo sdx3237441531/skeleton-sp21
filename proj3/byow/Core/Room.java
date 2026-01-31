7824f491d79af024e8d64ed327f5b6465b117580
sdx3237441531 <3237441531@qq.com>
2026-01-31 17:20:40 +0800

    Create Room.java

---
+++ proj3/byow/Core/Room.java
@@ -0,0 +1,48 @@
+package byow.Core;
+
+public class Room implements Comparable<Room> {
+    private int xOff;
+    private int yOff;
+    private int width;
+    private int height;
+
+    public Room(int xOff, int yOff, int width, int height) {
+        this.xOff = xOff;
+        this.yOff = yOff;
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public int compareTo(Room o) {
+        if (o.xOff > this.xOff) {
+            return -1;
+        } else if (o.xOff < this.xOff) {
+            return 1;
+        } else {
+            if (o.yOff > this.yOff) {
+                return -1;
+            } else if (o.yOff < this.yOff){
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    public int getxOff() {
+        return xOff;
+    }
+
+    public int getyOff() {
+        return yOff;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
