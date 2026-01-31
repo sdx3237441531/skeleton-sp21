e5d2f356d8cf10d9e79da70fc638c5ed4e63a864
sdx3237441531 <3237441531@qq.com>
2026-01-31 17:20:50 +0800

    Update Main.java

--- proj3/byow/Core/Main.java
+++ proj3/byow/Core/Main.java
@@ -1,5 +1,7 @@
 package byow.Core;
 
+// 用户如何启动整个系统。读取命令行参数并调用 Engine.java 中的相应函数
+
 /** This is the main entry point for the program. This class simply parses
  *  the command line inputs, and lets the byow.Core.Engine class take over
  *  in either keyboard or input string mode.
@@ -13,9 +15,9 @@ public class Main {
             Engine engine = new Engine();
             engine.interactWithInputString(args[1]);
             System.out.println(engine.toString());
-        // DO NOT CHANGE THESE LINES YET ;)
-        } else if (args.length == 2 && args[0].equals("-p")) { System.out.println("Coming soon."); } 
-        // DO NOT CHANGE THESE LINES YET ;)
+            // 请暂时不要更改这些代码行 ;)
+        } else if (args.length == 2 && args[0].equals("-p")) { System.out.println("Coming soon."); }
+        // 请暂时不要更改这些代码行 ;)
         else {
             Engine engine = new Engine();
             engine.interactWithKeyboard();
