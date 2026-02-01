94286b27dfe25acfbb45f6fb66cd7fbd13ec7edf
sdx3237441531 <3237441531@qq.com>
2026-02-01 10:24:50 +0800

    Create DownAction.java

---
+++ proj3/byow/Core/DownAction.java
@@ -0,0 +1,19 @@
+package byow.Core;
+
+import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
+
+public class DownAction implements Action{
+    @Override
+    public void action(World w, Avatar avatar) {
+        int x = avatar.getX();
+        int y = avatar.getY();
+        TETile[][] world = w.getWorld();
+        if (world[x][y - 1].character() != '#') {
+            world[x][y] = Tileset.FLOOR;
+            world[x][y - 1] = Tileset.AVATAR;
+            avatar = new Avatar(x, y - 1);
+            w.setAvatar(avatar);
+        }
+    }
+}
