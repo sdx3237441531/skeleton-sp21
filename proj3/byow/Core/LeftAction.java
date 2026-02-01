f738f58963f049cfb6c1114fb0403bf7609bdfc1
sdx3237441531 <3237441531@qq.com>
2026-02-01 10:49:54 +0800

    Update LeftAction.java

--- proj3/byow/Core/LeftAction.java
+++ proj3/byow/Core/LeftAction.java
@@ -3,7 +3,7 @@ package byow.Core;
 import byow.TileEngine.TETile;
 import byow.TileEngine.Tileset;
 
-public class LeftAction implements Action{
+public class LeftAction implements Action {
     @Override
     public void action(World w, Avatar avatar) {
         int x = avatar.getX();
