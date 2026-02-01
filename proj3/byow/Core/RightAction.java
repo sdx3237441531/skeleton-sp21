d0923d22b2b605945a95c1f17e7bc675b1245786
sdx3237441531 <3237441531@qq.com>
2026-02-01 10:49:49 +0800

    Update RightAction.java

--- proj3/byow/Core/RightAction.java
+++ proj3/byow/Core/RightAction.java
@@ -3,7 +3,7 @@ package byow.Core;
 import byow.TileEngine.TETile;
 import byow.TileEngine.Tileset;
 
-public class RightAction implements Action{
+public class RightAction implements Action {
     @Override
     public void action(World w, Avatar avatar) {
         int x = avatar.getX();
