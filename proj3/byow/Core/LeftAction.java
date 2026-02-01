95a63f6a32df3283b0bd110622f48708bb8cd564
sdx3237441531 <3237441531@qq.com>
2026-02-01 10:24:40 +0800

    Create LeftAction.java

---
+++ proj3/byow/Core/LeftAction.java
@@ -0,0 +1,19 @@
+package byow.Core;
+
+import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
+
+public class LeftAction implements Action{
+    @Override
+    public void action(World w, Avatar avatar) {
+        int x = avatar.getX();
+        int y = avatar.getY();
+        TETile[][] world = w.getWorld();
+        if (world[x - 1][y].character() != '#') {
+            world[x][y] = Tileset.FLOOR;
+            world[x - 1][y] = Tileset.AVATAR;
+            avatar = new Avatar(x - 1, y);
+            w.setAvatar(avatar);
+        }
+    }
+}
